4644d0de0e3dbb36d99a559121d1d298dc7b0e01
adam <bcuz@users.noreply.github.com>
2019-04-02 10:25:27 -0400

    setState w/ api data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    friends: []
+  }
+
+  componentDidMount() {
+    axios
+    .get('http://localhost:5000/friends')
+    .then(response => this.setState({friends: response.data}))
+    .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div>        
